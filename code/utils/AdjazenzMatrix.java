f6870aa4adb557e7bffe8994b0d191a69c5706d7
felix <felix.moehler@icloud.com>
2022-12-21 21:49:00 +0100

    added constructor2 in AdjazenzMatrix

--- code/utils/AdjazenzMatrix.java
+++ code/utils/AdjazenzMatrix.java
@@ -5,9 +5,17 @@ public class AdjazenzMatrix {
   private int[][] matrix;
   private String label;
   private boolean isDirected;
+  private int max_flow;
 
   public AdjazenzMatrix(String label, int[][] matrix, boolean isDirected) {
     this.matrix = matrix;
+    this.label = label;
+    this.isDirected = isDirected;
+  }
+
+  //constructor for ProblemTest2
+  public AdjazenzMatrix(String label, int max_flow, boolean isDirected) {
+    this.max_flow = max_flow;
     this.label = label;
     this.isDirected = isDirected;
   }
@@ -39,5 +47,10 @@ public class AdjazenzMatrix {
 
   public boolean isDirected() {
     return this.isDirected;
+  }
+
+  //getter for ProblemTest2
+  public int getMaxFlow() {
+    return this.max_flow;
   }
 }
